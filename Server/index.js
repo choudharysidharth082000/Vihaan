8e22755adf156953d787d549e6807c150d9068ef
SIDHARTH CHOUDHARY <choudharysidharth082000@gmail.com>
2022-03-11 23:03:42 +0530

    Boiler plate code

---
+++ Server/index.js
@@ -0,0 +1,38 @@
+console.log('Backend Sever Started');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const mongodb = require('mongodb');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+const app = express();
+
+dotenv.config();
+
+
+//cors and body parser 
+app.use(cors());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+
+//connection of the database
+
+
+
+
+
+app.listen(process.env.PORT, () =>
+{
+    console.log(`${process.env.PORT} Connected SuccessFully`)
+})
+
+
+
+
+
